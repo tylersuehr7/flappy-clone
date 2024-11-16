6e3cab8ff862368ef4fed75b1de5df3a71f6e85e
Tyler Suehr <tylersuehr@gmail.com>
2024-11-16 09:10:30 -0500

    ref to World + 2 constructor options

--- src/player.cpp
+++ src/player.cpp
@@ -1,4 +1,15 @@
 #include "player.hpp"
 
-Player::Player(const Rectangle bounds): m_bounds(bounds), m_velocity(0.0f) {
+Player::Player(const World &world): 
+    m_world(world), 
+    m_bounds((Rectangle) {.height=10, .width=10}), 
+    m_velocity(0.0f) {
+    m_bounds.x = (world.size.x / 2) - m_bounds.width;
+    m_bounds.y = (world.size.y / 2) - m_bounds.height;
+}
+
+Player::Player(const World &world, const Rectangle bounds):
+    m_world(world),
+    m_bounds(bounds),
+    m_velocity(0.0f) {
 }
--- src/player.hpp
+++ src/player.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "game.hpp"
 #include <raylib.h>
 
 class Player {
 private:
+    const World &m_world;
     Rectangle m_bounds;
     float m_velocity;
 public:
-    Player() = default;
-    Player(const Rectangle bounds);
+    Player(const World &world);
+    Player(const World &world, const Rectangle bounds);
 };
