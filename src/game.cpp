08ffae50861af16bce51d9d839a2b3d202caf605
Tyler Suehr <tylersuehr@gmail.com>
2024-11-16 08:58:57 -0500

    Component for managing core game logic

---
+++ src/game.cpp
@@ -0,0 +1,4 @@
+#include "game.hpp"
+
+Game::Game(): m_score(0), m_game_over(false), m_started(false) {
+}
---
+++ src/game.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <raylib.h>
+
+class Game {
+private:
+    bool m_game_over;
+    bool m_started;
+    int m_score;
+public:
+    Game();
+};
